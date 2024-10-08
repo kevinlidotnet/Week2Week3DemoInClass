3dfd4c072b4783f949faa7da75be508986c41962
KevinLi19 <kevin.li@georgiancollege.ca>
2024-10-08 16:50:03 -0400

    view improvements

--- src/main/java/com/example/demoinclass/Main.java
+++ src/main/java/com/example/demoinclass/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("covidCaseChartView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 540);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Food Menu Sunday");
         stage.setScene(scene);
         stage.show();
